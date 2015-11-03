6f0ca7d918e5d00590cddc75c1f0ef8349171aeb
Arturo Martinez Minguez <arturo@icemobile.com>
2015-11-03 13:42:07 +0100

    Externalized example

---
+++ example/api.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var util = require('util');
+var Emese = require('../lib/emese');
+
+var api = {
+    version: '0.0.1',
+    log: console.log,
+    debug: function (obj) {
+        api.log(util.inspect(obj, { depth: null, colors: true }));
+    }
+};
+
+var loader = new Emese(api);
+
+loader.resources({
+    client: {
+        rest: 'example/client/rest/*.js',
+        soap: 'example/client/soap/*.js'
+    },
+    common: 'example/common/*.js',
+    plugin: 'example/plugin/*.js',
+    service: 'example/service/*.js',
+    transformer: 'example/transformer/*.js'
+});
+
+loader.addTask([
+    function (next) {
+        api.log('Opening database connection');
+        setTimeout(function () {
+            api.log('Connected to the database');
+            next();
+        }, 150);
+    },
+    function (next) {
+        api.log('Initializing cache');
+        setTimeout(function () {
+            api.log('Cache initialized');
+            next();
+        }, 200);
+    },
+    function (next) {
+        api.plugin.server.load(next);
+    }
+]);
+
+loader.resources({
+    public: 'example/public/**/*.json'
+});
+
+
+loader.load();
+
+loader.on('error', function (err) {
+    api.log('ERROR!');
+    api.log(err);
+});
+
+loader.on('done', function (api) {
+    api.log('Your application is ready to serve');
+    api.debug(api);
+});
--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,10 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
 gulp.task('lint', function () {
-    return gulp.src('./lib/**/*.js')
+    return gulp.src([
+        './lib/**/*.js',
+        './example/**/*.js'
+    ])
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail'));
